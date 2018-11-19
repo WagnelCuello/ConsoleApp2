dd5d04632d956e707e1e94fbf65a3fb9c3ede936
wagnel <wagnelcuello@gmail.com>
2018-11-19 11:54:12 -0400

    primera subida

---
+++ ConsoleApp2/Problemas/Class1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2.Problemas
+{
+    public class Class1
+    {
+        /// <summary>
+        /// este metodo se encarga de mostrar el calculo de un perimetro de un cuadrado calculando sus lados
+        /// </summary>
+        public static void GetProblema1()
+        {
+            Console.WriteLine("===> CALCULO DE LADOS DE UN CUADRADO <===");
+            Console.Write("Ingrese el lado del cuadrado: ");
+            double lado = Convert.ToDouble(Console.ReadLine());
+
+            double perimetro = lado + 4;
+
+            Console.WriteLine("El perimetro del cuadrado es de {}", perimetro);
+            Console.ReadKey();
+        }
+    }
+}
